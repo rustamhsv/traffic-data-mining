da89bf194f79afcc5229d1942e38584c61b33b65
rustamhsv <rustamhsv@gmail.com>
2022-04-27 15:39:45 +0200

    Correlation between TOMTOM and individual POIs

--- Correlation/poi_tomtom_corr.py
+++ Correlation/poi_tomtom_corr.py
@@ -3,6 +3,14 @@ import json
 import scipy.io
 from collections import defaultdict
 from os import listdir
+# from scipy.stats.stats import pearsonr   
+from scipy import stats
+import seaborn as sb
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import random
+
 
 
 def get_hour_from_interval(hour):
@@ -16,45 +24,114 @@ def convert_time_to_sec(time):
 	return seconds_sum
 
 
-# df_tomtom = pd.read_excel('Moricz-Kelenfold_4_weeks.xlsx', skiprows=114)
-# df_tomtom.columns = df_tomtom.iloc[0]
-# df_tomtom = df_tomtom[1:]
+# place_id = 'ChIJhyxyHLXdQUcR6X3xu4va9kQ'
 
-# hours = list(df_tomtom['Time Set'])
-# travel_times = list(df_tomtom['Average Travel Time [hh:mm:ss]'])
+df = pd.read_csv('corr_result_pois.csv')
+place_ids = df['place_id']
 
-# hours_dict_tomtom = defaultdict(list)
-# for hour, travel_time in zip(hours, travel_times):
-# 	seconds_sum = convert_time_to_sec(travel_time)
-# 	hour = get_hour_from_interval(hour)
-# 	hours_dict_tomtom[hour].append(seconds_sum)
 
-hours_dict_poi = defaultdict(list)
-
-for file in listdir():
-	if 'Onlive' not in file:
+for place_id, node_type in zip(place_ids, df['node_type']):
+	if node_type != 'bar':
 		continue
 
-	df_poi = pd.read_csv(file)
-	place_id = 'ChIJ46y3BrPdQUcRcPB_NMbzbDU'
+	""" GET TomTom data for every hour - for every hour 20 data points (4 weeks) """
+	df_tomtom = pd.read_excel('Moricz-Kelenfold_4_weeks.xlsx', skiprows=114)
+	df_tomtom.columns = df_tomtom.iloc[0]
+	df_tomtom = df_tomtom[1:]
 
-	poi_filter = df_poi['place_id'] == place_id
-	df_poi = df_poi.loc[poi_filter]
+	hours = list(df_tomtom['Time Set'])
+	# travel_times = list(df_tomtom['Average Travel Time [hh:mm:ss]'])
+	travel_times = list(df_tomtom['Average Travel Time ratios'])
 
-	for poi in df_poi['popular_times']:
-		poi = poi.replace("'", '"')
-		poi_json = json.loads(poi)
-		# print(poi_json)
-		for day in range(5):
-			daily_data = poi_json[day]['data']
-			for hour, data in enumerate(daily_data):
-				hours_dict_poi[hour].append(data)
+	hours_dict_tomtom = defaultdict(list)
+	for hour, travel_time in zip(hours, travel_times):
+		# seconds_sum = convert_time_to_sec(travel_time)
+		hour = get_hour_from_interval(hour)
+		# hours_dict_tomtom[int(hour)].append(seconds_sum)
+		hours_dict_tomtom[int(hour)].append(travel_time)
+
+
+
+	""" GET TomTom data for every hour - for every hour 20 data points (4 weeks) """
+	hours_dict_poi = defaultdict(list)
+
+	for file in listdir():
+		if 'Onlive' not in file:
+			continue
+
+		df_poi = pd.read_csv(file)
+		poi_filter = df_poi['place_id'] == place_id
+		df_poi = df_poi.loc[poi_filter]
+
+		for poi in df_poi['popular_times']:
+			poi = poi.replace("'", '"')
+			poi_json = json.loads(poi)
+			# print(poi_json)
+			for day in range(5):
+				daily_data = poi_json[day]['data']
+				for hour, data in enumerate(daily_data):
+					hours_dict_poi[hour].append(data)
+
+	# hours_dict_tomtom[9] = [x for x in range(10, 1000, 100)][:20]
+	# print(len(hours_dict_tomtom[0]), len(hours_dict_poi[0]))
+
+
+	# print('TOMTOM:', len(hours_dict_tomtom[9]))
+	# print('POI:', len(hours_dict_poi[9]))
+
+	corr_matrix = []
+	for hour in range(0, 24):
+		try:
+			corr_result = stats.pearsonr(hours_dict_tomtom[hour], hours_dict_poi[hour])
+			corr_matrix.append(corr_result[0])
+		except ValueError:
+			continue
+	corr_matrix = [0 if math.isnan(coef) else coef for coef in corr_matrix]
+	sum_of_coefs = sum(map(abs, corr_matrix))
+	print(place_id, sum_of_coefs)
+	# print(len(hours_dict_poi.keys()), len(corr_matrix))
+
+	if sum_of_coefs > 5 and len(corr_matrix)==24:
+		plt.scatter(hours_dict_poi.keys(), corr_matrix)
+		plt.axis([0, 25, -1, 1])
+		plt.xlabel("HOURS")
+		plt.ylabel(f"CORR VALUE {node_type}")
+		plt.grid()
+		plt.show()
+
+
+
+
+# print(len(corr_matrix))
+# plt.scatter(hours_dict_tomtom.keys(), corr_matrix)
+# plt.axis([0, 25, -1, 1])
+# plt.xlabel("HOURS")
+# plt.ylabel("CORR VALUE")
+# plt.grid()
+# plt.show()
+
+# df = pd.DataFrame(corr_matrix)
+# print(df)
+# corr = df.corr()
+# print(corr)
+# data = np.random.rand(24,2)
+# df = pd.DataFrame(data) # Your data 
+# print(df)
+# corr = df.corr() # Calculates correlation matrix
+# sb.heatmap(corr, cmap="Blues", annot=True)
+
+
+# corr.style.background_gradient(cmap='coolwarm')
+
+
+# a = stats.spearmanr(hours_dict_tomtom[10], hours_dict_poi[10])
+# print(a)
 		# 	print(daily_data)
 		# 	weekly_data += daily_data
 		# weekly_data_pois.append(weekly_data)
 
-for h in hours_dict_poi.values():
-	print(len(h))
+# for h in hours_dict_poi.values():
+# 	print(len(h))
 
 # hours_dict = {}
 # for hour in hours:
